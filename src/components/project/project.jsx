5dfd25a312968ec284a91e74f8de4cbc9eeb377f
anupw19 <131799762+anupw19@users.noreply.github.com>
2024-10-27 23:22:31 +0530

    modified project

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -7,8 +7,7 @@ const items = [
     id: 1,
     title: "GEMINI-CLONE",
     img: "gemini.png",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
-  },
+    desc: "A Gemini Clone website is a replica of the popular chatbot interface, Gemini, powered by the Google Gemini API. These websites aim to replicate the functionality of the original Gemini platform, providing a user-friendly interface for interacting with the powerful Gemini model."},
   {
     id: 2,
     title: "title 2",
